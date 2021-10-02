32599d5a58143662c8d1c60861891b30e5cfa07b
vignesh-nethaji <vigneshnethajichengam@gmail.com>
2021-10-02 15:51:18 +0530

    Added base project

---
+++ OOPs.AccessModifier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPs.AccessModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
