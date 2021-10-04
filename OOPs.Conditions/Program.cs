bb1acf3ba9f52dbbb9d45cb24b8b72c87eb0c373
Vignesh Nethaji <off.vignesh@gmail.com>
2021-10-04 08:33:23 -0700

    Init Commit by Kalai

--- OOPs.Conditions/Program.cs
+++ OOPs.Conditions/Program.cs
@@ -6,7 +6,21 @@ namespace OOPs.Conditions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ///<summary>
+            ///  This is main function. Only here Method is should be calling.
+            ///  I created two object there for the access the class reference one is ConditionalsStatement 
+            /// </summary>
+
+            ConditionalStatement obj = new ConditionalStatement();
+
+            obj.Ifconditional();
+            obj.Elseifconditional();
+            obj.NextedIfConitional();
+            obj.SwitchConditional();
+            obj.Ternaryoperator();
+            obj.BreakConditional();
+            obj.ContinueConditional();
+
         }
     }
 }
